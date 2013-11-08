f396b72bb926d113080909558da0d61e39c4f4ea
Marty Peck <mwp@mwpnet.com>
2013-11-08 14:30:53 -0500

    added copyright to everything

--- root/javascript/drawPathHandles.js
+++ root/javascript/drawPathHandles.js
@@ -1,3 +1,21 @@
+/** Copyright 2013 Martyn Peck (mwp@mwpnet.com) **/
+
+/**    This file is part of drawing-to-code.									**/
+/**																				**/
+/**    drawing-to-code is free software: you can redistribute it and/or modify	**/
+/**    it under the terms of the GNU General Public License as published by		**/
+/**    the Free Software Foundation, either version 3 of the License, or		**/
+/**    (at your option) any later version.										**/
+/**																				**/
+/**    drawing-to-code is distributed in the hope that it will be useful,		**/
+/**    but WITHOUT ANY WARRANTY; without even the implied warranty of			**/
+/**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			**/
+/**    GNU General Public License for more details.								**/
+/**																				**/
+/**    You should have received a copy of the GNU General Public License		**/
+/**    along with drawing-to-code.  If not, see <http://www.gnu.org/licenses/>.	**/
+
+
 /********************************************
  * dwraPathHandles.js
  * 
