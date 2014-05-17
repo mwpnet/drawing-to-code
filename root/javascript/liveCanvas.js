ea572e4ff092d36b3f93db7fe2e7a93506fb6c29
Marty Peck <mwp@mwpnet.com>
2014-05-17 16:44:33 -0400

    need to go back and update the code parser before I can continue with this

--- root/javascript/liveCanvas.js
+++ root/javascript/liveCanvas.js
@@ -94,7 +94,7 @@ function init(){
 	hideButton.onclick = hideControlHandles;
 	initColor(context, canvas );
 	initLineProp();
-	initFillStrokeProp();
+	//initFillStrokeProp();
 	initFontProp();
 	initShadowProp();
 }
